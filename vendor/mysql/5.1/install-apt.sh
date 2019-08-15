be35b024a7d124134b7142b5337406c2e95b249f
Concurrent <C.Y.Concurrent@gmail.com>
2019-08-15 12:30:18 +0800

    init

---
+++ vendor/mysql/5.1/install-apt.sh
@@ -0,0 +1,3 @@
+deb http://packages.dotdeb.org lenny all
+sudo apt-get update
+apt-get install mysql-server-5.1
---
+++ vendor/mysql/5.1/install-wget.sh
@@ -0,0 +1,2 @@
+wget https://downloads.mysql.com/archives/get/file/mysql-5.1.73-linux-x86_64-glibc23.tar.gz
+tar -zxvf mysql-5.1.73-linux-x86_64-glibc23.tar.gz
